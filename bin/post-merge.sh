56e4198967f8b8ee97e3cc14a58265190288926d
Ken West <ken.west@internode.on.net>
2019-02-05 14:31:25 +1100

    Clear the Drupal caches for the default site, not reaching.city

--- bin/post-merge.sh
+++ bin/post-merge.sh
@@ -5,4 +5,4 @@
 drush vset nikadevs_cms_layout_cbf2019 --format=json - < nikadevs_cms_layout_cbf2019.json
 drush vset nikadevs_cms_layout_cbootf  --format=json - < nikadevs_cms_layout_cbf2019.json
 
-drush -l reaching.city cc all
+drush cc all
