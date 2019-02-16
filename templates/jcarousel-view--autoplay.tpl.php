3c6de6a5f80157988bb26bcff701241df36b2ca3
Ken West <ken.west@internode.on.net>
2019-02-16 13:53:29 +1100

    Turn om autoplay for OWL carousel views with the .autoplay class

---
+++ templates/jcarousel-view--autoplay.tpl.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * @file jcarousel-view.tpl.php
+ *
+ * View template to display a list as an OWL carousel.
+ *
+ * @TODO: Develop OWL carousel as a style plugin of its own. Don't
+ * need the full-blown Drupal OWL Carousel module as all the JS etc
+ * is dupplied by the Rhythm theme.
+ */
+?>
+<div class="slider-wrapper" data-autoplay="6000">
+  <div class="fullwidth-slider owl-carousel owl-theme">
+    <?php foreach ($rows as $id => $row): ?>
+      <div class="<?php print $row_classes[$id]; ?>">
+        <?php print $row; ?>
+      </div>
+    <?php endforeach; ?>
+  </div>
+</div>
