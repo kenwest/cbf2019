d6959827a870c652556b56b39b330118c9bed400
Ken West <ken.west@internode.on.net>
2019-03-12 15:20:36 +1100

    Add CSS and JS to display a Google Map with multiple markers. The views use the class map-canvas-view-multiple to mark this type of map.

--- js/cbf2019.js
+++ js/cbf2019.js
@@ -53,6 +53,109 @@
         });
       });
 
+      $('.map-canvas-view-multiple', context).each(function() {
+        var gmZoom = $(this).data('zoom') ? $(this).data('zoom') : 14;
+        var gmMarkers = [];
+        var gmLat;
+        var gmLong;
+        var gmIcon;
+        var gmContent;
+
+        $('.marker', this).each(function() {
+          gmLat = $(this).attr("data-lat");
+          gmLong = $(this).attr("data-long");
+          gmIcon = $(this).attr("data-icon");
+          gmContent = this.innerHTML;
+
+          gmMarkers.push({
+            latLng: [gmLat, gmLong],
+            data: gmContent,
+            options: {
+              icon: gmIcon
+            }
+          });
+        });
+
+        if (gmMarkers.length == 0) {
+          return;
+        }
+
+        $(this).gmap3({
+          action: "init",
+          map: {
+            options: {
+              zoom: gmZoom,
+              center: [gmLat, gmLong],
+              zoomControl: true,
+              zoomControlOptions: {
+                style: google.maps.ZoomControlStyle.SMALL,
+                position: google.maps.ControlPosition.RIGHT_CENTER
+              },
+              mapTypeControl: false,
+              scaleControl: false,
+              scrollwheel: false,
+              streetViewControl: false,
+              draggable: true,
+              styles: [
+                {"featureType":"water","elementType":"geometry.fill","stylers":[{"color":"#d3d3d3"}]},
+                {"featureType":"transit","stylers":[{"color":"#808080"},{"visibility":"off"}]},
+                {"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"visibility":"on"},{"color":"#b3b3b3"}]},
+                {"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ffffff"}]},
+                {"featureType":"road.local","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#ffffff"},{"weight":1.8}]},
+                {"featureType":"road.local","elementType":"geometry.stroke","stylers":[{"color":"#d7d7d7"}]},
+                {"featureType":"poi","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#ebebeb"}]},
+                {"featureType":"administrative","elementType":"geometry","stylers":[{"color":"#a7a7a7"}]},
+                {"featureType":"road.arterial","elementType":"geometry.fill","stylers":[{"color":"#ffffff"}]},
+                {"featureType":"road.arterial","elementType":"geometry.fill","stylers":[{"color":"#ffffff"}]},
+                {"featureType":"landscape","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#efefef"}]},
+                {"featureType":"road","elementType":"labels.text.fill","stylers":[{"color":"#696969"}]},
+                {"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"visibility":"on"},{"color":"#737373"}]},
+                {"featureType":"poi","elementType":"labels.icon","stylers":[{"visibility":"off"}]},
+                {"featureType":"poi","elementType":"labels","stylers":[{"visibility":"off"}]},
+                {"featureType":"road.arterial","elementType":"geometry.stroke","stylers":[{"color":"#d6d6d6"}]},
+                {"featureType":"road","elementType":"labels.icon","stylers":[{"visibility":"off"}]},
+                {"featureType":"poi","elementType":"geometry.fill","stylers":[{"color":"#dadada"}]}
+              ]
+            }
+          },
+          marker: {
+            values: gmMarkers,
+            options:{
+              draggable: false
+            },
+            events: {
+              mouseover: function(marker, event, context){
+                var map = $(this).gmap3("get"),
+                  infowindow = $(this).gmap3({get:{name:"infowindow"}});
+                if (infowindow) {
+                  infowindow.open(map, marker);
+                  infowindow.setContent(context.data);
+                } else {
+                  $(this).gmap3({
+                    infowindow: {
+                      anchor:marker,
+                      options:{content: context.data}
+                    }
+                  });
+                }
+              },
+              mouseout: function(){
+                var infowindow = $(this).gmap3({get:{name:"infowindow"}});
+                if (infowindow){
+                  infowindow.close();
+                }
+              }
+            }
+          }
+        });
+
+        if (gmMarkers.length > 1) {
+          $(this).gmap3({
+            autofit: {}
+          });
+        }
+      });
+
       $(".fullwidth-slider-view", context).each(function() {
         var autoplay = $(this).closest('.slider-wrapper').data('autoplay')
 
