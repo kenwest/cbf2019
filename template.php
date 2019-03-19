5d86654b673d407a34af5c79f63921797df2e17e
Ken West <ken.west@internode.on.net>
2019-03-19 21:08:22 +1100

    Load Rubik font

--- template.php
+++ template.php
@@ -4,5 +4,12 @@
  * Implementation of hook_preprocess_html().
  */
 function cbf2019_preprocess_html(&$variables) {
-  drupal_add_css(drupal_get_path('theme', 'cbf2019') . '/css/custom.css', array('group' => CSS_THEME));
+  drupal_add_css(
+    'https://fonts.googleapis.com/css?family=Rubik:300,300i,400,400i,500',
+    array('group' => CSS_THEME)
+    );
+  drupal_add_css(
+    drupal_get_path('theme', 'cbf2019') . '/css/custom.css',
+    array('group' => CSS_THEME)
+  );
 }
