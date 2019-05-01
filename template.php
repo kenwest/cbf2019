26c0deef8e501f1f2ac3e6b8f241d2fe3bfad792
Ken West <ken.west@internode.on.net>
2019-05-01 14:49:57 +1000

    Format pager on view displays  - use FontAwesome 5 icons  - make icons larger

--- template.php
+++ template.php
@@ -20,3 +20,59 @@ function cbf2019_preprocess_html(&$variables) {
 function cbf2019_entity_property($variables) {
   return cbf_entity_property($variables);
 }
+
+/*
+ * An implementation of theme_pager_link()
+ *
+ * It's a copy of rhythm_pager_link() but ...
+ *  - uses FontAwesome 5 icons
+ */
+function cbf2019_pager_link($variables) {
+  $text = $variables['text'];
+  $page_new = $variables['page_new'];
+  $element = $variables['element'];
+  $parameters = $variables['parameters'];
+  $attributes = $variables['attributes'];
+
+  $page = isset($_GET['page']) ? $_GET['page'] : '';
+  if ($new_page = implode(',', pager_load_array($page_new[$element], $element, explode(',', $page)))) {
+    $parameters['page'] = $new_page;
+  }
+
+  $query = array();
+  if (count($parameters)) {
+    $query = drupal_get_query_parameters($parameters, array());
+  }
+  if ($query_pager = pager_get_query_parameters()) {
+    $query = array_merge($query, $query_pager);
+  }
+
+  // Set each pager link title
+  if (!isset($attributes['title'])) {
+    static $titles = NULL;
+    if (!isset($titles)) {
+      $titles = array(
+        t('«') => t('Go to first page'),
+        t('‹') => t('Go to previous page'),
+        t('›') => t('Go to next page'),
+        t('»') => t('Go to last page'),
+      );
+    }
+    if (isset($titles[$text])) {
+      $attributes['title'] = $titles[$text];
+    }
+    elseif (is_numeric($text)) {
+      $attributes['title'] = t('Go to page @number', array('@number' => $text));
+    }
+  }
+  $replace_titles = array(
+    t('« first') => '<i class="fal fa-angle-double-left"></i>',
+    t('‹ previous') => '<i class="fal fa-angle-left"></i>',
+    t('next ›') => '<i class="fal fa-angle-right"></i>',
+    t('last »') => '<i class="fal fa-angle-double-right"></i>',
+  );
+  $text = isset($replace_titles[$text]) ? $replace_titles[$text] : check_plain($text);
+
+  $attributes['href'] = url($_GET['q'], array('query' => $query));
+  return '<a' . drupal_attributes($attributes) . '>' . $text . '</a>';
+}
