172ed09667fddde2f61013100c61775a42cc7112
Ken West <ken.west@internode.on.net>
2019-12-07 22:31:45 +1100

    Fix: Notice: Use of undefined constant domain_id - assumed 'domain_id' in include() (line 33 of .../themes/cbf2019/templates/views-view--page-header.tpl.php).

--- templates/views-view--page-header.tpl.php
+++ templates/views-view--page-header.tpl.php
@@ -30,7 +30,7 @@
 
 <div id="sticky-wrapper" class="sticky-wrapper" style="height: 75px;">
   <?php
-    switch ($domainId = domain_get_domain()[domain_id]){
+    switch ($domainId = domain_get_domain()['domain_id']){
       case '4':
         $mainMenuName = 'menu-cbf-2019-plus-menu';
         break;
