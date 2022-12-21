4009ca95b61cea0a61c32eade40e3fbf5c58f22f
kenwest <west.ken.au@gmail.com>
2022-12-21 20:13:43 +1100

    Code and format the Article Topic Hierarchy as a Rhythm Accordion

---
+++ templates/views-view-unformatted--cbf2019-article-topics--block-1.tpl.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * @file
+ * Default simple view template to display a list of rows.
+ *
+ * - $title : The title of this group of rows.  May be empty.
+ * - $options['type'] will either be ul or ol.
+ * @ingroup views_templates
+ */
+?>
+<dt>
+    <a href="#">
+      <?php print $title; ?>
+    </a>
+  </dt>
+  <dd style="display: block;">
+    <?php foreach ($rows as $id => $row): ?>
+      <div class="clearfix <?php print $classes_array[$id]; ?>"><?php print $row; ?></div>
+    <?php endforeach; ?>
+  </dd>
+</dt>
