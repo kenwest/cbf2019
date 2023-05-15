b0a6009881f5aed1e5c07b03a36a632d5d05b638
kenwest <west.ken.au@gmail.com>
2023-05-15 18:24:41 +1000

    Add code for embedding HubSpot or DepositFix forms as a field in a node

---
+++ templates/field--field-hubspot-form.tpl.php
@@ -0,0 +1,80 @@
+<?php
+
+/**
+ * Available variables:
+ * - $items: An array of field values. Use render() to output them.
+ * - $label: The item label.
+ * - $label_hidden: Whether the label display is set to 'hidden'.
+ * - $classes: String of classes that can be used to style contextually through
+ *   CSS. It can be manipulated through the variable $classes_array from
+ *   preprocess functions. The default values can be one or more of the
+ *   following:
+ *   - field: The current template type, i.e., "theming hook".
+ *   - field-name-[field_name]: The current field name. For example, if the
+ *     field name is "field_description" it would result in
+ *     "field-name-field-description".
+ *   - field-type-[field_type]: The current field type. For example, if the
+ *     field type is "text" it would result in "field-type-text".
+ *   - field-label-[label_display]: The current label position. For example, if
+ *     the label position is "above" it would result in "field-label-above".
+ *
+ * Other variables:
+ * - $element['#object']: The entity to which the field is attached.
+ * - $element['#view_mode']: View mode, e.g. 'full', 'teaser'...
+ * - $element['#field_name']: The field name.
+ * - $element['#field_type']: The field type.
+ * - $element['#field_language']: The field language.
+ * - $element['#field_translatable']: Whether the field is translatable or not.
+ * - $element['#label_display']: Position of label display, inline, above, or
+ *   hidden.
+ * - $field_name_css: The css-compatible field name.
+ * - $field_type_css: The css-compatible field type.
+ * - $classes_array: Array of html class attribute values. It is flattened
+ *   into a string within the variable $classes.
+ *
+ * @see template_preprocess_field()
+ * @see theme_field()
+ *
+ * @ingroup themeable
+ */
+
+  foreach ($items as $_ => $item) {
+    $hubspotForm = reset($item['entity']['paragraphs_item']);
+
+    $formType = cbf_field_get_items('paragraphs_item', $hubspotForm['#entity'], 'field_hubspot_form_type', 'tid', false);
+    $formId = cbf_field_get_items('paragraphs_item', $hubspotForm['#entity'], 'field_title', 'value', false);
+  }
+
+  $formType = taxonomy_term_load($formType);
+  $formType = $formType->name;
+
+  switch ($formType) {
+
+    case 'HubSpot':
+      ?>
+        <script charset="utf-8" type="text/javascript" src="//js.hsforms.net/forms/embed/v2.js"></script>
+        <script>
+          hbspt.forms.create({
+            region: "na1",
+            portalId: "<?php print variable_get('hubspot_portal_id'); ?>",
+            formId: "<?php print $formId; ?>"
+          });
+        </script>
+      <?php
+      break;
+
+    case 'DepositFix':
+      ?>
+        <script id='df-widget-js' src='https://widgets.depositfix.com/v1/app.min.js?v2'></script>
+        <script id='df-script' type='text/javascript'>
+          DepositFixForm.init({
+            formId: '<?php print $formId; ?>',
+            portalId: '<?php print variable_get('depositfix_portal_id'); ?>'
+          });
+        </script>
+      <?php
+      break;
+
+    default:
+      break;
+  }
