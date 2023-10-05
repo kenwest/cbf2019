9abdda4c7842ed502b4b9945e142f8defd0d1bde
kenwest <west.ken.au@gmail.com>
2023-10-05 20:24:01 +1100

    Fix code to use $itemOutput rather than $itemModified as input to preg_replace. Simplify code.

--- templates/field--field-staff-member.tpl.php
+++ templates/field--field-staff-member.tpl.php
@@ -55,18 +55,8 @@
         }
 
         $itemOutput = render($item);
-        $matches = null;
-        $itemModified = preg_replace(
-          '/ (href=")/i',
-          ' rel="nofollow" $1',
-          $itemModified
-        );
-        if (!empty($itemModified)) {
-          print $itemModified;
-        }
-        else {
-          print $itemOutput;
-        }
+        $itemModified = preg_replace('/ (href=")/i', ' rel="nofollow" $1', $itemOutput) ?? $itemOutput;
+        print $itemModified;
       }
 
       if ($itemCount > 0) {
